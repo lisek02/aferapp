c622eb568928cc654216b3de63a3745f2f44ea15
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2015-03-10 23:14:05 +0100

    editing user profile without authorization

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,11 +15,7 @@ describe "Authentication" do
 
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
-			before do
-				fill_in "Email", 		with: user.email
-				fill_in "Password", 	with: user.password
-				click_button "Log in"
-			end
+			before { log_in user }
 
 			it { should have_title(full_title(user.login)) }
 			it { should have_link('Profile',			href: user_path(user)) }
